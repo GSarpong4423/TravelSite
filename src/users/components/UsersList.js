7d129a8c51120a3d274d88df2e43e4041af8781b
GSarpong4423 <sarpongg@gmail.com>
2020-09-06 23:14:09 -0400

    Added UserList Components and updated image

--- src/users/components/UsersList.js
+++ src/users/components/UsersList.js
@@ -3,18 +3,17 @@ import React from 'react';
 import UserItem from './UserItem';
 import './UsersList.css'
 
-const UseraList = props => {
-    if (props.items.lenght ===0){
+const UsersList = props => {
+    if (props.items.length ===0){
         return (
             <div className="center">
               <h2> No users found.</h2>
         </div>
         );
     }
-}
     return (
-        <ul>
-            {props.items.map(user=>(<UserItems 
+        <ul className="users-lists">
+            {props.items.map(user=>(<UserItem 
             key ={user.id} 
             image={user.image} 
             name={user.name}
@@ -23,7 +22,9 @@ const UseraList = props => {
             ))}
         </ul>
     );
+};
+    
         
-                    
+                
         
-export default UseraList;
+export default UsersList;
--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -5,7 +5,7 @@ import UsersList from '../components/UsersList';
 const Users = () => {
     const USERS =[{id: 'u1', 
     name: 'Georgina S', 
-    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.123rf.com%2Fstock-photo%2Fafrican_american_avatar.html&psig=AOvVaw2OpOuH2qgjlFMX5JSI-W1M&ust=1599532408625000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMiJ-tmA1usCFQAAAAAdAAAAABAG',places: 3}
+    image: 'https://media.istockphoto.com/photos/portrait-of-a-confident-young-architect-in-a-modern-office-picture-id1251518064',places: 3}
 ];
 
     return <UsersList items={USERS} />;
