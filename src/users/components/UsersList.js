531661edfe2e7043ec0c15a6147b084e6d79134f
GSarpong4423 <sarpongg@gmail.com>
2020-09-09 08:27:12 -0400

    Main Header part 1 - setting up files and adding components

--- src/users/components/UsersList.js
+++ src/users/components/UsersList.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
 import UserItem from './UserItem';
+import Card from '../../shared/components/UIElements/Card';
 import './UsersList.css'
+
+
 
 //stateful component
 const UsersList = props => {
     if (props.items.length ===0){
         return (
             <div className="center">
+              <Card>
               <h2> No users found.</h2>
+              </Card>
         </div>
         );
     }
