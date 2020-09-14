523d6fdd683cd98a617ef65ec4d0bac5f43a6d2d
GSarpong4423 <sarpongg@gmail.com>
2020-09-14 16:08:05 -0400

    Added css to places files and added object (props) to userList file for out put

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES = [
+    {
+    id:'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.great-towers.com%2Ftower%2Fempire-state-building&psig=AOvVaw3msACNA_W9oPZz2JZAzDyE&ust=1600372440637000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLDHn4-67usCFQAAAAAdAAAAABAD',
+    address: '20 W 34th St, New York, NY 10001',
+    loction:{
+        lat: 40.7484405,
+        lng: -73.9856644
+        }
+        creator: 'u1'
+   },
+   {
+    id:'p2',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.great-towers.com%2Ftower%2Fempire-state-building&psig=AOvVaw3msACNA_W9oPZz2JZAzDyE&ust=1600372440637000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLDHn4-67usCFQAAAAAdAAAAABAD',
+    address: '20 W 34th St, New York, NY 10001',
+    loction:{
+        lat: 40.7484405,
+        lng: -73.9856644
+        }
+
+   }
+
+];
+
 const UserPlaces = () => {
-    return 
+    return <PlaceList items={DUMMY_PLACES} />;
 };
 
 export default UserPlaces;
